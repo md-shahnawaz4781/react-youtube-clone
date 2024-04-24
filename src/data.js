59b04233269e7f48e7f7ef51fe5656e97da86449
md-shahnawaz4781 <shahnawaz4781@gmail.com>
2024-04-24 19:50:39 +0530

    remove the api key

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'AIzaSyC6H-LV8rPhFcZlRf6woHC0jueDXZEZ6VE'; // Remove the API_KEY and insert your here
+export const API_KEY = '[Enter Your API Key here]'
 
 export const value_converter = (value)=>{
         if(value>=1000000){
